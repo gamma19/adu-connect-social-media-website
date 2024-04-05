87fd054e187f0a3813a5ab6f4d981772f9d46296
Utku Enes Baki <61944226+gamma19@users.noreply.github.com>
2024-04-05 17:36:07 +0300

    button update
    button update

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -204,22 +204,31 @@ const Post = (props) => {
           </div>
         </div>
         <div className="comment-input">
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={toggleCommentInput}
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.2 }}
+            className=""
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="white"
-              class="bi bi-chat-fill"
-              viewBox="0 0 16 16"
+            <button
+              type="submit"
+              class="btn btn-primary"
+              onClick={toggleCommentInput}
             >
-              <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9 9 0 0 0 8 15" />
-            </svg>
-          </Button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="white"
+                class="bi bi-chat-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9 9 0 0 0 8 15" />
+              </svg>
+            </button>
+          </motion.div>
           {showCommentInput && (
             <div className="comment-input-inside">
               <TextField
@@ -230,18 +239,31 @@ const Post = (props) => {
                 onChange={handleSendComment}
                 fullWidth
               />
-              <Button variant="contained" color="primary" onClick={sendComment}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-send-fill"
-                  viewBox="0 0 16 16"
+              <motion.div
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.2 }}
+                className=""
+              >
+                <button
+                  type="submit"
+                  class="btn btn-primary"
+                  onClick={sendComment}
                 >
-                  <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
-                </svg>
-              </Button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-send-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
+                  </svg>
+                </button>
+              </motion.div>
             </div>
           )}
         </div>
