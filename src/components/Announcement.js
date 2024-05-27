23229630bda2aea720ff6a22a15478705ef71c44
Utku Enes Baki <baki.utku@hotmail.com>
2024-05-27 19:26:02 +0300

    announcement updates

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -6,14 +6,21 @@ import AnnouncementFragment from "../components/announcement-fragment/Announceme
 import authHeader from "../services/auth-header";
 import { useState, useEffect } from "react";
 import AuthService from "../services/auth.service";
+import { TextField } from "@mui/material";
+import axios from "axios";
+import { motion } from "framer-motion";
 
 const Announcement = () => {
   const [announcement, setAnnouncement] = useState([]);
   //const [user, setUser] = useState(null);
   const [user, setUser] = useState({ username: "" });
   const [admin, setAdmin] = useState(false);
-
+  const [titleError, setTitleError] = useState(false);
+  const [title, setPostTitle] = useState("");
+  const [icerik, setIcerik] = useState("");
+  const [icerikError, setIcerikError] = useState(false);
   const currentUser = AuthService.getCurrentUser();
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const isAdmin = currentUser.roles.some((role) => role === "ROLE_ADMIN");
@@ -32,6 +39,33 @@ const Announcement = () => {
         }
       );
   }, []);
+
+  const handleTitleChange = (e) => {
+    setPostTitle(e.target.value);
+    setTitleError(e.target.value.trim() === "");
+  };
+
+  const handleIcerikChange = (e) => {
+    setIcerik(e.target.value);
+    setIcerikError(e.target.value.trim() === "");
+  };
+
+  const handleSubmit = (e) => {
+    const postData = {
+      title: title,
+      icerik: icerik,
+      userId: currentUser.id,
+    };
+
+    axios
+      .post(`/posts`, postData, { headers: authHeader() })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error submitting form:", error);
+      });
+  };
 
   console.log(admin);
 
@@ -55,6 +89,68 @@ const Announcement = () => {
             <header></header>
             <div className="announcement-main">
               <div className="announcement-container">
+                <form className="post-form" onSubmit={handleSubmit}>
+                  <TextField
+                    type="text"
+                    id="outlined-multiline-static"
+                    label="Başlığı Gir"
+                    multiline
+                    rows={1}
+                    fullWidth
+                    value={title}
+                    onChange={handleTitleChange}
+                    required
+                  />
+                  {titleError && (
+                    <div className="error-message" style={{ color: "red" }}>
+                      Lütfen duyuru başlığını giriniz.
+                    </div>
+                  )}
+                  <TextField
+                    label="İçeriği Gir"
+                    id="outlined-multiline-static"
+                    multiline
+                    rows={1}
+                    fullWidth
+                    value={icerik}
+                    onChange={handleIcerikChange}
+                    required
+                  />
+
+                  {icerikError && (
+                    <div className="error-message" style={{ color: "red" }}>
+                      Lütfen duyuru içeriğini giriniz.
+                    </div>
+                  )}
+                  <motion.div
+                    whileHover={
+                      !titleError && !icerikError ? { scale: 1.1 } : {}
+                    }
+                    whileTap={!titleError && !icerikError ? { scale: 0.9 } : {}}
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.2 }}
+                    className=""
+                  >
+                    <button
+                      type="submit"
+                      class="btn btn-primary"
+                      disabled={titleError || icerikError}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-send-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
+                      </svg>
+                      &nbsp; Duyuruyu Gönder
+                    </button>
+                  </motion.div>
+                </form>
                 {announcement
                   .slice()
                   .reverse()
--- src/components/announcement-fragment/AnnouncementFragment.js
+++ src/components/announcement-fragment/AnnouncementFragment.js
@@ -27,63 +27,7 @@ const AnnouncementFragment = (props) => {
               {icerik}
             </p>
           </div>
-          <div className="announcement-fragment-bottom">
-            <div className="announcement-fragment-date">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-calendar2"
-                viewBox="0 0 16 16"
-              >
-                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
-                <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z" />
-              </svg>
-              &nbsp; Tarih
-            </div>
-            <div className="announcement-fragment-time">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-alarm"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z" />
-                <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z" />
-              </svg>
-              &nbsp; Saat
-            </div>
-            <div className="announcement-fragment-readcount">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-people-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-              </svg>
-              &nbsp; Okunma Sayısı
-            </div>
-            <div className="announcement-fragment-read">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-eye-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
-              </svg>
-              &nbsp; Okundu
-            </div>
-          </div>
+          {/* <div className="announcement-fragment-bottom"></div>*/}
         </div>
       </div>
     </>
